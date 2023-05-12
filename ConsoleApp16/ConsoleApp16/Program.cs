5a5b6c582a9fba2c8726d3cc98a84de256d9873f
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2023-05-12 11:05:31 +0300

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace FileWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader text = new StreamReader(@"C:\Users\Admin\OneDrive\Рабочий стол\file\file.txt");
+            string[] array = text.ReadToEnd().Split('\n');
+            foreach (var i in array)
+                try
+                {
+                    Console.WriteLine(i);
+
+                    StreamReader text = new StreamReader(@"C:\Users\Admin\OneDrive\Рабочий стол\file\file.txt");
+                    string[] array = text.ReadToEnd().Split('\n');
+                    foreach (var i in array)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                catch (OutOfMemoryException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+        }
+    }
+}
